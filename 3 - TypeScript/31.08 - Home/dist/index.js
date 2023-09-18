bcffa72edb549d6b442b44dcfeb87ac985e2b260
Miriam Kisos <87091443+MKisos30@users.noreply.github.com>
2023-09-18 21:57:28 +0300

    regex

--- 3 - TypeScript/31.08 - Home/dist/index.js
+++ 3 - TypeScript/31.08 - Home/dist/index.js
@@ -51,14 +51,71 @@ sortUpBtn.addEventListener("click", function () {
 // 4. Create an input of search by author/name of book and show the result in the DOM
 // (choose which one of them to search to).
 var searchFun = function (ev) {
-    //console.log(ev)
-    // console.log(ev.target.value)
     var inputValue = ev.target.value;
-    console.log(inputValue);
-    // const searchArray = bookArray.filter((book) => {
-    //     book.nameOfBook == inputValue
-    // })
     var searchArray = bookArray.filter(function (book) { return book.nameOfBook.includes(inputValue); });
-    // console.log(aaa)
     return render(searchArray);
 };
+// create select next to the input that include aouthr name and book name
+// give to the user option to find the book by one of them
+// show the founded book/books in the DOM (use render function).
+// 1. לעשות הוספת ספר חדש לרשימה
+// 1.1. לעשות regex בזמן אמת על האינפוטים, במידה והמשתמש מכניס טקסט לא נכון
+// (למשל איפה שצריך להיות מספרים מכניס טקסט
+// להעיר את האינפוט בצבע אדום, במידה והטקסט נכון, האינפוט מתהפך לצבע ירוק.
+var getValueInput = function (ev) {
+    ev.preventDefault();
+    var nameOfBook = ev.target.elements.nameOfBook.value;
+    var nameVal = /[a-zA-Z]/i;
+    var matchName = nameVal.test(nameOfBook);
+    var authorName = ev.target.elements.authorName.value;
+    var authorNameVal = /[a-zA-Z]/i;
+    var matchAuthor = authorNameVal.test(authorName);
+    var yearOfPublish = ev.target.elements.yearOfPublish.value;
+    var yearOfPublishVal = /[0-9]/;
+    var matchYear = yearOfPublishVal.test(yearOfPublish);
+    if (matchName && matchAuthor && matchYear) {
+        bookArray.push({ nameOfBook: nameOfBook, author: authorName, yearOfPublish: yearOfPublish });
+        return render(bookArray);
+    }
+    else {
+        alert('Something is not correct');
+    }
+};
+var checkInput = function (ev) {
+    try {
+        var input = ev.target;
+        var typeOfInput = ev.target.type;
+        var valueOfInput = ev.target.value;
+        var textVal = /[a-zA-Z]/i;
+        var numVal = /[0-9]/;
+        var check = void 0;
+        if (typeOfInput == "text") {
+            check = textVal.test(valueOfInput);
+        }
+        else if (typeOfInput == "number") {
+            check = numVal.test(valueOfInput);
+        }
+        else {
+            console.log("there is no type like this");
+        }
+        if (check) {
+            return input.style.background = "green";
+        }
+        else if (valueOfInput.length == 0) {
+            return input.style.background = "none";
+        }
+        else {
+            return input.style.background = "red";
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+};
+/*
+2. Add types of ts to:
+2.1. function (what the function get in parameters and what it return),
+2.2. objects,
+2.3. events,
+2.4. queryselectors
+*/ 
--- 3 - TypeScript/31.08 - Home/index.ts
+++ 3 - TypeScript/31.08 - Home/index.ts
@@ -76,16 +76,79 @@ sortUpBtn.addEventListener("click",()=>{
 // (choose which one of them to search to).
 
 const searchFun = (ev) => {
-    //console.log(ev)
-
-    // console.log(ev.target.value)
     const inputValue = ev.target.value
-    console.log(inputValue)
-    // const searchArray = bookArray.filter((book) => {
-    //     book.nameOfBook == inputValue
-    // })
-   const searchArray = bookArray.filter((book) => book.nameOfBook.includes(inputValue))
-
-    // console.log(aaa)
+    const searchArray = bookArray.filter((book) => book.nameOfBook.includes(inputValue))
     return render(searchArray)
 }
+
+// create select next to the input that include aouthr name and book name
+// give to the user option to find the book by one of them
+// show the founded book/books in the DOM (use render function).
+
+// 1. לעשות הוספת ספר חדש לרשימה
+
+// 1.1. לעשות regex בזמן אמת על האינפוטים, במידה והמשתמש מכניס טקסט לא נכון
+// (למשל איפה שצריך להיות מספרים מכניס טקסט
+// להעיר את האינפוט בצבע אדום, במידה והטקסט נכון, האינפוט מתהפך לצבע ירוק.
+
+
+const getValueInput = (ev) => {
+  ev.preventDefault()
+
+  const nameOfBook = ev.target.elements.nameOfBook.value
+  const nameVal: RegExp = /[a-zA-Z]/i
+  const matchName = nameVal.test(nameOfBook)
+
+  const authorName = ev.target.elements.authorName.value
+  const authorNameVal: RegExp = /[a-zA-Z]/i
+  const matchAuthor = authorNameVal.test(authorName)
+
+  const yearOfPublish = ev.target.elements.yearOfPublish.value
+  const yearOfPublishVal: RegExp = /[0-9]/
+  const matchYear = yearOfPublishVal.test(yearOfPublish)
+
+  if (matchName && matchAuthor && matchYear) {
+      bookArray.push({nameOfBook, author: authorName, yearOfPublish})
+      return render(bookArray)
+  } else {
+    alert('Something is not correct')
+  }
+
+}
+
+const checkInput = (ev) =>{
+  try {
+    const input = ev.target
+    const typeOfInput = ev.target.type
+    const valueOfInput = ev.target.value
+    const textVal: RegExp = /[a-zA-Z]/i
+    const numVal: RegExp = /[0-9]/
+
+    let check 
+    if(typeOfInput == "text") {
+      check = textVal.test(valueOfInput)
+    } else if (typeOfInput == "number") {
+      check = numVal.test(valueOfInput)
+    } else {
+      console.log(`there is no type like this`)
+    }
+
+    if(check) {
+      return input.style.background = "green"
+    } else if (valueOfInput.length == 0) {
+      return input.style.background = "none"
+    } else {
+      return input.style.background = "red"
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+/* 
+2. Add types of ts to:
+2.1. function (what the function get in parameters and what it return), 
+2.2. objects, 
+2.3. events, 
+2.4. queryselectors
+*/
