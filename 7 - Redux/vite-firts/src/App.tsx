f013b44d64a6c2b4cd022e5d4d64c1182c844f1f
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2024-04-19 15:12:17 +0300

    redux with reducers

--- 7 - Redux/vite-firts/src/App.tsx
+++ 7 - Redux/vite-firts/src/App.tsx
@@ -1,14 +1,27 @@
+import { useState } from 'react';
 import './App.css';
-import { selectCount } from "./app/counter/counterSlice";
-import { useAppSelector } from "./app/hooks";
+import { decrement, increment, incrementByAmount, selectCount, setValueZero } from "./app/counter/counterSlice";
+import { useAppDispatch, useAppSelector } from "./app/hooks";
 
 function App() {
   const count = useAppSelector(selectCount)
-  return <main>
+  const dispatch = useAppDispatch()
+  const [newAmount, setNewAnount] = useState<number>(0)
+  // console.log(`new Amount: ${newAmount}`)
+
+  return (
+  <main>
     <h2>Vite + Redux</h2>
     <div>{count}</div>
-    <button>Add more</button>
+    <button onClick={() => dispatch(increment())}>Add more</button>
+    <button onClick={() => dispatch(setValueZero())}>set zore</button>
+    <button onClick={() => dispatch(decrement())}>Minus 1</button>
+    <div>
+      <input type="number" defaultValue={newAmount} onChange={(ev: any) => setNewAnount(ev.target.value)} />
+      <button onClick={() => dispatch(incrementByAmount(Number(newAmount))) }>Add new Amount</button>
+    </div>
   </main>
+  )
 }
 
 export default App;
